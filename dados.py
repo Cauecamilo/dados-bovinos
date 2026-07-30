6df09db6b450a6874349dedabf76c209bd6b1b13
Cauê Camilo <cauecamilo9@gmail.com>
2026-07-30 11:25:48 -0300

    Inseri as Raças

--- dados.py
+++ dados.py
@@ -1,5 +1,22 @@
 
-RACAS = {}
+RACAS = {
+    "Nelore": {
+        "gmd_min": 0.8, "gmd_max": 1.2,
+        "rendimento_min": 0.52, "rendimento_max": 0.55
+    },
+    "Angus": {
+        "gmd_min": 1.2, "gmd_max": 1.6,
+        "rendimento_min": 0.54, "rendimento_max": 0.57
+    },
+    "Guzerá": {
+        "gmd_min": 0.75, "gmd_max": 1.1,
+        "rendimento_min": 0.51, "rendimento_max": 0.54
+    },
+    "Girolando": {
+        "gmd_min": 0.5, "gmd_max": 0.8,
+        "rendimento_min": 0.45, "rendimento_max": 0.48
+    },
+}
 CUSTO_VACINA_AFTOSA = 0
 PERDA_TRANSPORTE_PADRAO = 0
 FUNRURAL_PF = 0
